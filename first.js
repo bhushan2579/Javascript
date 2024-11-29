f3de684e963045f629da29078047faab3d89c2b0
Buddhabhushan Adsule <buddhabhushansr457tehs@gmail.com>
2024-11-29 07:08:23 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,91 @@
+// console.log("Hello JS")
+// fname="Buddhabhushan Adsule"
+// console.log(fname)
+// age=19
+// console.log(age) 
+
+//      fname="bhushan",
+//      age=19,
+//      lname="Adsule"
+
+
+//     fname="ujwal" ,
+//     age=20,
+//     lname="Adsuel"
+
+//     console.log(fname)
+//     console.log(age)
+//     console.log(lname)
+// FullName = "Yash Dayal";
+// age = 24;
+// Age = 50;
+// price = 99.99;
+// x = undefined ;
+// $jonny="MVP"
+// isfollow= false ;
+// console.log(FullName)
+// console.log(age)
+// console.log(Age)
+// console.log(price)
+// console.log(x)
+// console.log($jonny)
+// console.log(isfollow)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Student = {
+    fullName : "Rahul",
+    age: 20,
+    marks : 82,
+    isPass : true
+};
+console.log(Student);
+console.log(Student["fullName"]);
+
+
+let a = 3
+let b = 5 
+
+console.log(a)
+console.log(b)
+
+// let c = a
+// console.log(c)
+a = b
+console.log(a)
+ b= a
+console.log(b)
