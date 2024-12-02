356e792e10af72ca2cf2f566041431e64c11d328
Buddhabhushan Adsule <buddhabhushansr457tehs@gmail.com>
2024-12-02 07:50:36 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,67 +1,66 @@
-// console.log("Hello JS")
-// fname="Buddhabhushan Adsule"
-// console.log(fname)
-// age=19
-// console.log(age) 
+ console.log("Hello JS")
+ fname="Buddhabhushan Adsule"
+ console.log(fname)
+ age=19
+ console.log(age) 
 
-//      fname="bhushan",
-//      age=19,
-//      lname="Adsule"
+      fname="bhushan",
+      age=19,
+      lname="Adsule"
 
+    fname="ujwal" ,
+     age=20,
+     lname="Adsuel"
 
-//     fname="ujwal" ,
-//     age=20,
-//     lname="Adsuel"
-
-//     console.log(fname)
-//     console.log(age)
-//     console.log(lname)
-// FullName = "Yash Dayal";
-// age = 24;
-// Age = 50;
-// price = 99.99;
-// x = undefined ;
-// $jonny="MVP"
-// isfollow= false ;
-// console.log(FullName)
-// console.log(age)
-// console.log(Age)
-// console.log(price)
-// console.log(x)
-// console.log($jonny)
-// console.log(isfollow)
-
-
-// const Student = {
-//     fullName : "Rahul Patil",
-//     age: 20,
-//     marks : 82,
-//     isPass : true
-// };
-// console.log(Student);
-// console.log(Student["fullName"]);
-// console.log(Student["age"]);
-// console.log(Student["marks"]);
-// console.log(Student["isPass"]);
+     console.log(fname)
+     console.log(age)
+     console.log(lname)
+ FullName = "Yash Dayal";
+ age = 24;
+ Age = 50;
+ price = 99.99;
+ x = undefined ;
+ $jonny="MVP"
+ isfollow= false ;
+ console.log(FullName)
+ console.log(age)
+ console.log(Age)
+ console.log(price)
+ console.log(x)
+ console.log($jonny)
+ console.log(isfollow)
 
 
-// let a = 3
+ const Student = {
+     fullName : "Rahul Patil",
+     age: 20,
+     marks : 82,
+     isPass : true
+ };
+ console.log(Student);
+ console.log(Student["fullName"]);
+ console.log(Student["age"]);
+ console.log(Student["marks"]);
+ console.log(Student["isPass"]);
 
-// let b = 5 
 
-// console.log(a = a )
+ let a = 3
 
-// console.log(b = b )
+ let b = 5 
 
-// console.log("a = ",b," & b = ",a);
-//  a = a + 1 
-//  console.log(a)
-//  console.log(a * 3)
-//  console.log(a + 2)
-//  console.log(b - 5)
-//  console.log(b / 3)
-//  console.log(a ** 2 )
-//  console.log(b % 7)
+ console.log(a = a )
+
+ console.log(b = b )
+
+ console.log("a = ",b," & b = ",a);
+  a = a + 1 
+  console.log(a)
+  console.log(a * 3)
+  console.log(a + 2)
+  console.log(b - 5)
+  console.log(b / 3)
+  console.log(a ** 2 )
+  console.log(b % 7)
  
  
  
@@ -78,11 +77,11 @@
 
 
 //  Assignement Operator
-//   let k = 4;
-//   let l =5;
+   let k = 4;
+   let l =5;
    
-//   k != 4; 
-// console.log("k = ", k) ;
+   k != 4; 
+ console.log("k = ", k) ;
 
 // Comparison Operators
 let k = 4 ;
