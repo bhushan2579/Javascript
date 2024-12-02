5b4f650f9e74266d33b31cc262fd9a6aa1c7acf7
Buddhabhushan Adsule <buddhabhushansr457tehs@gmail.com>
2024-12-02 07:54:15 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,90 @@
+console.log("Hello JS")
+fname="Buddhabhushan Adsule"
+console.log(fname)
+age=19
+console.log(age) 
+
+     fname="bhushan",
+     age=19,
+     lname="Adsule"
+
+
+    fname="ujwal" ,
+    age=20,
+    lname="Adsuel"
+
+    console.log(fname)
+    console.log(age)
+    console.log(lname)
+FullName = "Yash Dayal";
+age = 24;
+Age = 50;
+price = 99.99;
+x = undefined ;
+$jonny="MVP"
+isfollow= false ;
+console.log(FullName)
+console.log(age)
+console.log(Age)
+console.log(price)
+console.log(x)
+console.log($jonny)
+console.log(isfollow)
+
+
+const Student = {
+    fullName : "Rahul Patil",
+    age: 20,
+    marks : 82,
+    isPass : true
+};
+console.log(Student);
+console.log(Student["fullName"]);
+console.log(Student["age"]);
+console.log(Student["marks"]);
+console.log(Student["isPass"]);
+
+
+let a = 3
+
+let b = 5 
+
+console.log(a = a )
+
+console.log(b = b )
+
+console.log("a = ",b," & b = ",a);
+ a = a + 1 
+ console.log(a)
+ console.log(a * 3)
+ console.log(a + 2)
+ console.log(b - 5)
+ console.log(b / 3)
+ console.log(a ** 2 )
+ console.log(b % 7)
+ 
+ 
+ 
+ //Unary Operator
+ let c = 9;
+ let d = 8;
+ console.log("c = ",c," & d = ",d)
+ console.log(c ++ )
+ console.log("c =", c) 
+ console.log(-- d)
+ console.log("d = ", d)
+
+
+
+
+//  Assignement Operator
+//    let k = 4;
+//   let l =5;
+//       k != 4; 
+//  console.log("k = ", k) ;
+
+// Comparison Operators
+let k = 4 ;
+let l = "4" ;
+ 
+console.log("4 != 4 ", k != l);
